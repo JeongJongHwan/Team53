a191841baae402cb9e161c703c51a96c3f3906fb
Leesangsu <sangsu333@kunsan.ac.kr>
2019-05-01 14:42:45 +0900

    init.

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,13 +1,23 @@
 package com.example.myapplication;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void login(View v) {
+        Toast.makeText(this,"로그인 화면으로 이동합니다.",Toast.LENGTH_LONG).show();
+        Intent intent = new Intent(this,LoginActivity.class);
+        startActivity(intent);
     }
 }
